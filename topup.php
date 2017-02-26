d9fb704f3a635302f92c54fa0e306b81659f9297
LouiseW853 <louisewatson940@gmail.com>
2017-02-26 18:04:09 +0000

    php files and queries used in the mobile app

---
+++ topup.php
@@ -0,0 +1,16 @@
+<?php
+
+include "db.php";
+
+$username = !empty($_POST['username']) ? trim($_POST['username']) : null;
+$topup = !empty($_POST['topup']) ? trim($_POST['topup']) : null;
+
+
+?>
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
